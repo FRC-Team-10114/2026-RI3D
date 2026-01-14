fbc4384d06fa71a6a56df0154f5d722bf5c97ac0
YuanChi Hsieh <60728633+boring89@users.noreply.github.com>
2026-01-14 13:08:48 +0800

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -71,6 +71,7 @@ public class RobotContainer {
 
     public RobotContainer() {
         autoChooser = AutoBuilder.buildAutoChooser();
+        autoChooser = AutoBuilder.buildAutoChooser("Tests");
         SmartDashboard.putData("Auto Mode", autoChooser);
 
         field = new Field2d();
